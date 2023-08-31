9fd8e15b8696d33f9ad6347170851f999302b757
Rubens Kaiserman <rubenskaiserman@gmail.com>
2023-08-31 18:29:00 -0300

    refactor

--- transcriber/serializers.py
+++ transcriber/serializers.py
@@ -4,4 +4,4 @@ from .models import Transcriber_Model_Template
 class Transcriber_Serializer_Template(serializers.ModelSerializer):
     class Meta:
         model = Transcriber_Model_Template
-        fields = ['email', 'name', 'vstorage_id']
+        fields = ['email', 'name']
